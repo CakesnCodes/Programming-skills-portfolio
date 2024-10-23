57e2b8c1f2d2c7be874d161e55a969c3478dc167
CakesnCodes <scribblechronicles@protonmail.com>
2024-10-23 20:20:30 +0400

    Update and rename Exercise 7 alt.py to Exercise 7.py

--- Exercise 7.py
+++ Exercise 7.py
@@ -8,19 +8,19 @@ print(" ")
 print("loop 50-0")   
 a=50 #create variable 'a' with value of 50
 for i in range(51): #Create loop with a range of 51
-    print(a) #print a
+    print(a) #display the value of a
     a -= 1 #subtract 1 from a
     
 print (" ")    
 print("loop 30-50")
 for i in range (30,51): #Create loop with a range of 51, starting at 30
-    print (i)    #Print current iteration
+    print (i)  #Print current iteration
     
 print (" ")
 print ("loop 50-10")
 b=50 #create variable 'b' with value of 50
 for i in range(21): #Create loop with a range of 21
-    print(b)
+    print(b) #display the value of b
     b -= 2 #subtract 2 from b
     
 print(" ")   
