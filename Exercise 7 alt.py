f4b4f693b9bb0e181ad6751651255cd345b797fc
CakesnCodes <scribblechronicles@protonmail.com>
2024-10-23 20:18:31 +0400

    Add files via upload
    OK DAS THE REAL ONE

--- Exercise 7 alt.py
+++ Exercise 7 alt.py
@@ -1,33 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 15 10:57:31 2024
+#Exercise 7 - Some Counting
 
-@author: User
-"""
 print("loop 0-50")
-for i in range(51):
-    print (i)
+for i in range(51): #Create loop with a range of 51
+    print (i) #Print current iteration
     
 print(" ")    
 print("loop 50-0")   
-a=50
-for i in range(51):
-    print(i)
-    a -= 1
+a=50 #create variable 'a' with value of 50
+for i in range(51): #Create loop with a range of 51
+    print(a) #print a
+    a -= 1 #subtract 1 from a
     
 print (" ")    
 print("loop 30-50")
-for i in range (30,51):
-    print (i)    
+for i in range (30,51): #Create loop with a range of 51, starting at 30
+    print (i)    #Print current iteration
     
 print (" ")
 print ("loop 50-10")
-b=50
-for i in range(21):
+b=50 #create variable 'b' with value of 50
+for i in range(21): #Create loop with a range of 21
     print(b)
-    b -= 2
+    b -= 2 #subtract 2 from b
     
 print(" ")   
 print("loop 100-200")    
-for i in range (100,201,5):
-    print(i)
+for i in range (100,201,5): #Create loop with a range of 51, starting from 100, and increase by 5
+    print(i)  #Print current iteration
